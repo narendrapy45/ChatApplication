5225859f6e81a9e91368ded397be0915b4dbe93f
“narendra45” <narendranaru45@gmail.com>
2021-03-03 23:33:27 +0530

    37.Asynchronously Loading Images with Javascript

--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -346,17 +346,16 @@ def calculate_timestamp(timestamp):
 
 
 class LazyRoomChatMessageEncoder(Serializer):
-	def get_dump_object(self, obj):
-		dump_object = {}
-		dump_object.update({'msg_type': MSG_TYPE_MESSAGE})
-		dump_object.update({'user_id': str(obj.user.id)})
-		dump_object.update({'username': str(obj.user.username)})
-		dump_object.update({'message': str(obj.content)})
-		dump_object.update({'profile_image': str(obj.user.profile_image.url)})
-		dump_object.update({'natural_timestamp': calculate_timestamp(obj.timestamp)})
-		return dump_object
-
-
+        def get_dump_object(self, obj):
+                dump_object = {}
+                dump_object.update({'msg_type': MSG_TYPE_MESSAGE})
+                dump_object.update({'msg_id': str(obj.id)}) # TODO: ADD THIS LINE.
+                dump_object.update({'user_id': str(obj.user.id)})
+                dump_object.update({'username': str(obj.user.username)})
+                dump_object.update({'message': str(obj.content)})
+                dump_object.update({'profile_image': str(obj.user.profile_image.url)})
+                dump_object.update({'natural_timestamp': calculate_timestamp(obj.timestamp)})
+                return dump_object
 
 
 
