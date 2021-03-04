5652e6f0036a335bec5c8b914318ba26f5a9c97a
“narendra45” <narendranaru45@gmail.com>
2021-03-04 12:30:17 +0530

    40.Private Chatroom View

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -41,6 +41,9 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
 
+    #Chat App
+    path('chat/', include('chat.urls', namespace='chat')),
+
      # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), 
         name='password_change_done'),
