d57d162c2db80fcabc550872933f610aef73cd42
“narendra45” <narendranaru45@gmail.com>
2021-03-04 12:56:45 +0530

    41.Creating Private Chatrooms

--- friendApp/models.py
+++ friendApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
+from chat.utils import find_or_create_private_chat
 
 
 class FriendList(models.Model):
@@ -19,6 +19,12 @@ class FriendList(models.Model):
 		if not account in self.friends.all():
 			self.friends.add(account)
 			self.save()
+			# Create a private chat (or activate an old one)
+			
+			chat = find_or_create_private_chat(self.user, account)
+			if not chat.is_active:
+				chat.is_active = True
+				chat.save()
 
 	def remove_friend(self, account):
 		"""
@@ -26,6 +32,12 @@ class FriendList(models.Model):
 		"""
 		if account in self.friends.all():
 			self.friends.remove(account)
+
+			# Deactivate the private chat between these two users
+			chat = find_or_create_private_chat(self.user, account)
+			if chat.is_active:
+				chat.is_active = False
+				chat.save()
 
 	def unfriend(self, removee):
 		"""
